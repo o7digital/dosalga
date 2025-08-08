831d22b3fde24447fa37616349a8ca90107d0a37
o7digital <101292703+o7digital@users.noreply.github.com>
2025-08-08 09:22:52 -0600

    fix(hero): usar imágenes del demo en bannerData

--- src/data/bannerData.js
+++ src/data/bannerData.js
@@ -3,7 +3,7 @@ export  const  banner1Data = [
         id:1,
         title:"Healthy skin cannot be rushed.",
         description:"Whatever your summer looks like, bring your own heat with up to 25% off Lumin Brand.",
-        banner_img:"/assets/img/home1/banner-right-bg.png",
+        banner_img:"https://beautico-nextjs.vercel.app/assets/img/home1/banner-right-bg.png",
         discount:"50",
         blackFriday:true
     },
@@ -11,7 +11,7 @@ export  const  banner1Data = [
         id:2,
         title:"Skin wellness takes gentle time.",
         description:"Whatever your summer looks like, bring your own heat with up to 25% off Lumin Brand.",
-        banner_img:"/assets/img/home1/banner-right-bg2.png",
+        banner_img:"https://beautico-nextjs.vercel.app/assets/img/home1/banner-right-bg2.png",
         discount:"23",
         blackFriday:false
     },
@@ -19,7 +19,7 @@ export  const  banner1Data = [
         id:3,
         title:"Glow requires gradual nurturing.",
         description:"Whatever your summer looks like, bring your own heat with up to 25% off Lumin Brand.",
-        banner_img:"/assets/img/home1/banner-right-bg3.png",
+        banner_img:"https://beautico-nextjs.vercel.app/assets/img/home1/banner-right-bg3.png",
         discount:"50",
         blackFriday:true
     },
@@ -27,7 +27,7 @@ export  const  banner1Data = [
         id:4,
         title:"Nurture skin for lasting health.",
         description:"Whatever your summer looks like, bring your own heat with up to 25% off Lumin Brand.",
-        banner_img:"/assets/img/home1/banner-right-bg4.png",
+        banner_img:"https://beautico-nextjs.vercel.app/assets/img/home1/banner-right-bg4.png",
         discount:"15",
         blackFriday:false
     }
