24b21d001820003f87e8f8bcf988f6381fbdd2ce
o7digital <101292703+o7digital@users.noreply.github.com>
2025-12-15 21:32:42 -0600

    Add Cookiebot script to document head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,13 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;600;700;800;900&family=Kalam:wght@700&display=swap"
           rel="stylesheet"
         />
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="0f81dffe-04f0-4f78-8b0e-5383f69567df"
+          data-blockingmode="auto"
+          type="text/javascript"
+        />
       </Head>
       <body>
         <Main />
